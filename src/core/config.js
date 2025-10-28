846e33a9039862c931853072abc6b916a5d035d0
Mykhailo Voronianskyi <voronyanskiy@gmail.com>
2025-10-28 19:35:40 +0200

    update to 2.10.2
    add main_icon in dader, change color scheme

--- src/core/config.js
+++ src/core/config.js
@@ -1,10 +1,10 @@
 // src/core/config.js
 
 /**
- * CONFIGURATION SETTINGS (v2.10.1)
+ * CONFIGURATION SETTINGS (v2.10.2)
  */
 export const CONFIG = {
-    APP_VERSION: "2.10.1", // ОНОВЛЕНО v2.10.1 (Refactoring style.css, Updating the Data Model (Backend))
+    APP_VERSION: "2.10.2", // ОНОВЛЕНО v2.10.2 (add main_icon in dader, change color scheme)
     AUTOSAVE_DELAY: 1500, // ms
     DEFAULT_GOAL_WORDS: 50000,
     SNIPPET_LENGTH: 80, // characters
